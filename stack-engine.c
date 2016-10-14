3575af1f1fb31adf603cef2d246435f8c40778a4
Chris <cclamb@unm.edu>
2016-10-14 04:43:56 -0600

    when build according to notes, heap-engine and stack-engine will spawn a shell

--- stack-engine.c
+++ stack-engine.c
@@ -9,7 +9,18 @@
 
 // This is the shellcode buffer. Insert shellcode into the
 // code variable here, for execution later.
-char shellcode[] = "\x55\x48\x89\xe5\x31\xc0\xc7\x45\xfc\x00\x00\x00\x00\xc7\x45\xf8\x05\x00\x00\x00\x5d\xc3";
+// This shellcode will start /bin/sh.
+char shellcode[] =
+    "\x48\x31\xd2"                                  // xor    %rdx, %rdx
+    "\x48\xbb\x2f\x2f\x62\x69\x6e\x2f\x73\x68"      // mov  $0x68732f6e69622f2f, %rbx
+    "\x48\xc1\xeb\x08"                              // shr    $0x8, %rbx
+    "\x53"                                          // push   %rbx
+    "\x48\x89\xe7"                                  // mov    %rsp, %rdi
+    "\x50"                                          // push   %rax
+    "\x57"                                          // push   %rdi
+    "\x48\x89\xe6"                                  // mov    %rsp, %rsi
+    "\xb0\x3b"                                      // mov    $0x3b, %al
+    "\x0f\x05";                                     // syscall
 
 int main(int argc, char *argv[]) {
   //exeshell is a function pointer
